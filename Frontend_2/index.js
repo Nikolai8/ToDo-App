b6878c174ae7d50db99db88d85ab1af305445747
Nikolai Mora Lozano <nikolai.moralozano@tecomon.com>
2022-11-22 21:06:05 +0100

    frontend_2 - add inputs when adding todos

--- Frontend_2/index.js
+++ Frontend_2/index.js
@@ -7,15 +7,30 @@ const ToDo = {
 
     init(){
         document.getElementById("ListAddItemContainer").addEventListener("click", () => {
-            this.addToDo("Test");
+            this.addToDo();
         });
     },
 
-    addToDo(des) {
+    addToDo() {
         let div = document.createElement("div");
         div.classList.add("ListItemContainer");
-        div.innerText = des;
+
+        let input = document.createElement("input");
+        input.classList.add("ListAddItemInput");
+        input.onkeydown = (evt) => {
+            if(evt.key == "Enter") {
+                evt.target.blur();
+                div.innerText = input.value;
+
+                this.saveToDo();
+            }
+        }
+        div.appendChild(input);
 
         document.getElementById("ListContainer").appendChild(div);
+    },
+
+    saveToDo() {
+        alert("fetch post ToDo");
     }
  }
