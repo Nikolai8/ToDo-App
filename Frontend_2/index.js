f7ea2989106be913d608c0127f8d1af9e6966ab5
Nikolai Mora Lozano <nikolai.moralozano@tecomon.com>
2022-11-23 16:05:19 +0100

    Frontend_2 - fix get, fix bugs

--- Frontend_2/index.js
+++ Frontend_2/index.js
@@ -13,13 +13,22 @@ const ToDo = {
 
         await fetch("http://127.0.0.1:8080/todos/")
         .then(res => {
-            console.error(res);
-
-            /*for(let todo in res) {
+            return res.json();
+        })
+        .then(data => {
+            for(let todo in data[0]){
                 let div = document.createElement("div");
                 div.classList.add("ListItemContainer");
+                div.innerText = todo;
+
+                let svg = document.createElement("svg");
+                svg.classList.add("checkToDoIcon");
+                svg.innerHTML = '<svg width="60" height="60" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg" > <path d="M10.5858 13.4142L7.75735 10.5858L6.34314 12L10.5858 16.2427L17.6568 9.1716L16.2426 7.75739L10.5858 13.4142Z" fill="#06c258" /> </svg>'
+                svg.addEventListener("click", () => this.deleteToDo(todo, svg.parentElement));
+
+                div.appendChild(svg);
                 document.getElementById("ListContainer").appendChild(div);
-            }*/
+            }
         });
     },
 
@@ -73,7 +82,7 @@ const ToDo = {
             headers: {'Content-Type': 'application/json'}
         })
         .then(res => {
-            console.error(res);
+            console.log(res);
         });
     },
 
@@ -85,7 +94,7 @@ const ToDo = {
             headers: {'Content-Type': 'application/json'}
         })
         .then(res => {
-            console.error(res);
+            console.log(res);
         });
     }
  }
