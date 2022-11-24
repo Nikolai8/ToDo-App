3d9abdb79c206a4f69fec0977fb9fc4ad7c08f05
Marc Brugger <mabrit11@hs-esslingen.de>
2022-11-24 16:15:15 +0100

    SQL Injection works

--- Backend/src/main/java/com/app/todo/ApiController.java
+++ Backend/src/main/java/com/app/todo/ApiController.java
@@ -76,10 +76,7 @@ public class ApiController {
 */
     @GetMapping(path="/")
     public @ResponseBody Iterable<TodoItem> getTodoItems() {
-        // This returns a JSON or XML with the users
-    //    ArrayList<TodoItem> items = new ArrayList<>();
-    //    todoRepo.findAll().forEach(items::add);
-    //    return items;
+        // returns a JSON or XML with the users
         return todoRepo.findAll();
     }
 
@@ -129,10 +126,9 @@ public class ApiController {
             Connection conn = DriverManager.getConnection(myUrl, "root", "todouser");
 
             // create the mysql delete statement.
-            // i'm deleting the row where the id is "3", which corresponds to my
-            // "Barney Rubble" record.
             //String query = "DELETE FROM todo.todo_item WHERE todo = \"?\"";
-            String query = "DELETE FROM todo.todo_item WHERE todo = " + "\"" + name + "\"";
+            String query = "DELETE FROM todo.todo_item WHERE todo IN (\"" + name + "\");";
+            System.out.println(query);
             PreparedStatement preparedStmt = conn.prepareStatement(query);
             //preparedStmt.setString(1, name);
             
@@ -148,127 +144,4 @@ public class ApiController {
         //todoRepo.deleteById(name);
         return "Item deleted";
     }
-
-    /*
-    @RestController
-    @Controller
-    @RequestMapping("/todos")
-    public class ApiController {
-
-    // internal list of todo items
-    @Autowired
-    private ArrayList<TodoItem> items = new ArrayList<TodoItem>();
-    // Add new item to list
-    // version 1: using path variables
-    @Operation(summary = "Creates a Todo Item with path variable name and default priority of 2")
-    @ApiResponses(value = 
-    {
-        @ApiResponse(responseCode = "201", description = "Item has been created" , content = @Content)
-    })
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/{name}")
-    public TodoItem createAndAddTodoItem(@PathVariable String name){
-
-        TodoItem item = new TodoItem(name);
-        items.add(item);
-
-        return item;
-    }
-
-    // Add new item to list
-    // version 2: using a JSON object as input
-    // TODO: Allow only one item with certain name in list.
-
-    // Be careful when importing RequestBody
-    @Operation(summary = "Creates a Todo Item with a JSON object as request paramter")
-    @ApiResponses(value = 
-    {
-        @ApiResponse(responseCode = "201", description = "Item has been created" , content = @Content)
-    })
-    @ResponseStatus(HttpStatus.CREATED)
-    @PostMapping("/")
-    public TodoItem addTodoItem(@RequestBody TodoItem item){
-
-        items.add(item);
-        return item;
-    }
-
-    // List all elements in ArrayList
-    @Operation(summary = "Returns a list of ToDo items")
-    @GetMapping(value = "/", produces = "application/json")
-    @ApiResponses(value = 
-                    {
-                        @ApiResponse(responseCode = "200", description = "List all items" , content = @Content)
-                    })
-    @ResponseStatus(HttpStatus.OK)
-    public List<TodoItem> getTodoItems(){
-
-        return items;
-    }
-
-    @Operation(summary = "Find a ToDo item by its itemId")
-    @ApiResponses(value = {
-            @ApiResponse(responseCode = "200", description = "Found the item", content = {
-                    @Content(mediaType = "application/json", schema = @Schema(implementation = TodoItem.class)) }),
-            @ApiResponse(responseCode = "400", description = "Invalid itemId supplied", content = @Content),
-            @ApiResponse(responseCode = "404", description = "Item not found", content = @Content) })
-    @GetMapping(produces = "application/json", path = "/{itemId}")
-    Optional<TodoItem> getTodoItem(@PathVariable String itemId) {
-
-        TodoItem tempItem = new TodoItem(itemId);
-        Optional<TodoItem> returnItem = Optional.empty();
-
-        for(TodoItem item : items){
-
-            if (item.equals(tempItem)) returnItem = Optional.of(item);
-
-        }
-        // TODO: Return 404 in case of item not found
-        return returnItem;
-
-    }
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping(produces = "application/json", path = "/{itemId}")
-    TodoItem deleteTodoItem(@PathVariable String itemId){
-
-        TodoItem tempItem = new TodoItem(itemId);
-        Iterator<TodoItem> iterator = items.iterator();
-
-        while(iterator.hasNext()){
-
-            if(iterator.next().equals(tempItem))
-                iterator.remove();
-
-        }
-
-        return null;
-    }
-
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    @DeleteMapping(consumes = "application/json", produces = "application/json", path = "/")
-    TodoItem deleteTodo(@RequestBody TodoItem item){
-
-        items.remove(item);
-
-        return null;
-    }
-
-    @ResponseStatus(HttpStatus.OK)
-    @PutMapping(consumes = "application/json", produces = "application/json", path = "/")
-    TodoItem updateTodo(@RequestBody TodoItem todoItem){
-
-        for(TodoItem item : items){
-
-            if (item.equals(todoItem)) item.setPriority(todoItem.getPriority());
-            return item;
-
-        }
-
-        items.add(todoItem);
-        return todoItem;
-
-    }
-    */
-    
 }
